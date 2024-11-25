af5ceb805fd01174534e897555d83639374d8442
CreativeBuilds <nick@creativebuilds.io>
2024-11-24 22:41:20 -0600

    updated port

--- anchors-edge/server/conf/settings.py
+++ anchors-edge/server/conf/settings.py
@@ -34,6 +34,7 @@ from evennia.settings_default import *
 # This is the name of your game. Make it catchy!
 SERVERNAME = "anchors-edge"
 START_LOCATION = "#3"
+TELNET_PORTS = [21602]
 
 ######################################################################
 # Settings given in secret_settings.py override those in this file.

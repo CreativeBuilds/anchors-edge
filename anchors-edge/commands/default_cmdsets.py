8ac684a93842059a2c474391e4b5f5e5b4a89551
CreativeBuilds <nick@creativebuilds.io>
2024-12-17 19:12:50 -0600

    feat(commands): Add CmdWhisper to CharacterCmdSet
    - Import CmdWhisper from commands.command
    - Add CmdWhisper to CharacterCmdSet

--- anchors-edge/commands/default_cmdsets.py
+++ anchors-edge/commands/default_cmdsets.py
@@ -25,7 +25,7 @@ from commands.admin import (
     CmdCleanupAccounts, CmdResetAccount, CmdDebugCharacter,
     CmdLastWipe, CmdChangelog, CmdResetWorld
 )
-from commands.command import CmdWho, CmdLook, CmdSay, CmdLsay, CmdHeight, CmdAge
+from commands.command import CmdWho, CmdLook, CmdSay, CmdLsay, CmdHeight, CmdAge, CmdWhisper
 from commands.emote import CmdEmote, CmdPmote, CmdOmote, CmdTmote
 from commands.unloggedin import CmdUnloggedinLook
 from commands.social import add_social_commands
@@ -74,6 +74,7 @@ class CharacterCmdSet(DefaultCharacterCmdSet):
         add_social_commands(self)
         self.add(CmdHeight())
         self.add(CmdAge())
+        self.add(CmdWhisper())
 
 
 class AccountCmdSet(DefaultAccountCmdSet):
